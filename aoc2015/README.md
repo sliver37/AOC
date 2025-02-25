5ac4f00bc95664a7476d7368d78fd7590f2ae8df
Yohan <john@handmadeweb.com.au>
2025-02-25 18:42:21 +1100

    Day 5

--- aoc2015/README.md
+++ aoc2015/README.md
@@ -14,8 +14,8 @@
 ![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
-![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
+[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
@@ -83,23 +83,23 @@ Both parts: 2.087ms
 
 ```
 Day 03
-Time part 1: 1.905ms
-Time part 2: 3.142ms
-Both parts: 5.047ms
+Time part 1: 1.686ms
+Time part 2: 2.035ms
+Both parts: 3.721ms
 ```
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 301.868ms
+Time part 2: 1240.059ms
+Both parts: 1541.927ms
 ```
 
 ```
 Day 05
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 24.351ms
+Time part 2: 0.792ms
+Both parts: 25.143ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 7.134ms
+Total stars: 8/50
+Total time: 1572.879ms
 ```
 
 <!--/RESULTS-->
---
+++ aoc2015/src/day05/index.ts
@@ -0,0 +1,75 @@
+import run from "aocrunner"
+
+const parseInput = (rawInput: string) => rawInput
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  let nice = 0
+
+  input.split("\n").forEach((line) => {
+    const testcases = [
+      line.match(/[aeiou]/g)?.length >= 3,
+      !/(ab|cd|pq|xy)/g.test(line),
+      /(.)\1/g.test(line),
+    ]
+
+    if (testcases.every((e) => e === true)) {
+      nice++
+    }
+  })
+
+  return nice
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  let nice = 0
+
+  input.split("\n").forEach((line) => {
+    const testcases = [
+      /(..).*?\1/.test(line),
+      /(.).\1/.test(line),
+    ]
+
+    if (testcases.every((e) => e === true)) {
+      nice++
+    }
+  })
+
+  return nice
+}
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `aeiouu`,
+        expected: 1,
+      },
+      {
+        input: `aeiouxy`,
+        expected: 0,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `qjhvhtzxzqqjkmpb`,
+        expected: 1,
+      },
+      {
+        input: `uurcxstgmygtbstg`,
+        expected: 0,
+      },
+      {
+        input: `ieodomkazucvgmuy`,
+        expected: 0,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+})
